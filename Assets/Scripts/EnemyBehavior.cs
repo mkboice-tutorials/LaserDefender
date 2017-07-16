2db733c10bfadbe8523cc45a0714f1ae3eea955f
Mike Boice <mike.boice@netapp.com>
2017-07-15 19:06:39 -0600

    Add Enemy projectiles

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -4,7 +4,26 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    public GameObject projectile;
     public float health = 150;
+    public float projectileSpeed = 10;
+    public float shotsPerSecond = 0.5f;
+
+    void Update()
+    {
+        float probability = Time.deltaTime * shotsPerSecond;
+        if (Random.value < probability)
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        Vector3 startPosition = transform.position + new Vector3(0f, -1f, 0f);
+        GameObject missile = Instantiate(projectile, startPosition, Quaternion.identity) as GameObject;
+        missile.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+    }
 
     void OnTriggerEnter2D(Collider2D col)
     {
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     public GameObject projectile;
     public float projectileSpeed;
     public float firingRate = 0.2f;
+    public float health = 250f;
 
     private float xMin;
     private float xMax;
@@ -27,7 +28,8 @@ public class PlayerController : MonoBehaviour
 
     void Fire()
     {
-        GameObject beam = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+        Vector3 offset = new Vector3(0, 1, 0);
+        GameObject beam = Instantiate(projectile, transform.position + offset, Quaternion.identity) as GameObject;
         beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0, projectileSpeed, 0);
     }
 
@@ -55,5 +57,20 @@ public class PlayerController : MonoBehaviour
         // Restrict the player to the gamespace
         float newX = Mathf.Clamp(transform.position.x, xMin, xMax);
         transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Projectile missile = col.gameObject.GetComponent<Projectile>();
+        if (missile)
+        {
+            health -= missile.GetDamage();
+            missile.Hit();
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+            Debug.Log("Player hit by a projectile");
+        }
     }
 }
